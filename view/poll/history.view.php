6fa649031ece083d52e86ca80103e2abf3bb79cd
Steen Munter <msmunter@gmail.com>
2018-01-17 13:37:20 -0800

    Load more button on the history page and a js file to handle it.

--- view/poll/history.view.php
+++ view/poll/history.view.php
@@ -1,3 +1,6 @@
 <div id="errorMsg"></div>
 <?php //$this->debug($this->pollSet); // DEBUG ONLY!!! ?>
 <?php include('view/poll/pollset.view.php'); ?>
+<div class="center">
+	<button data-inline="true" onclick="loadMoreRecentPolls()">Load More</button>
+</div>
---
+++ web/js/poll/history.js
@@ -0,0 +1,3 @@
+function loadMoreRecentPolls() {
+	alert('soon!');
+}
