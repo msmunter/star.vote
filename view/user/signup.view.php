0b8de9dc7ba2804287214ac805edb47385507713
Steen Munter <msmunter@gmail.com>
2018-04-02 16:02:54 -0700

    Proper display of polls on user page now.

--- view/user/signup.view.php
+++ view/user/signup.view.php
@@ -24,15 +24,6 @@
 			<label for="pass2">Verify Password:</label>
 			<input type="password" name="pass2" id="pass2" value="" />
 		</div>
-		<!--<div class="ui-field-contain">
-			<label for="adminLevel">Admin Level:</label>
-			<select name="adminLevel" id="adminLevel" data-inline="true">
-				<option value="0">None</option>
-				<option selected value="3">Employee</option>
-				<option value="2">Manager</option>
-				<option value="1">Root Admin</option>
-			</select>
-		</div>-->
 		<button type="button" data-inline="true" onclick="submitAddUserForm()">Add User</button>
 	</div>
 </div>
