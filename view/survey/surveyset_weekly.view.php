59d2ccd21f7c36bec0bf952a5e06388e73cf3615
Steen Munter <msmunter@gmail.com>
2018-09-20 23:18:18 +0000

    Verbage change indicating where to click.

--- view/survey/surveyset_weekly.view.php
+++ view/survey/surveyset_weekly.view.php
@@ -6,13 +6,13 @@ echo '<div class="recentOrPopularBar">Round One: 9/13 - 9/26</div>';
 
 echo '<a class="surveyLink" href="/eats/">';
 	echo '<div class="pollInfoVotes">Votes<br />'.$this->surveys->eats->votes.'</div>';
-	echo 'Eats';
+	echo 'Vote on Eats';
 	//echo '<div class="pollInfo">Started '.$survey->created.'</div>';
 echo '</a>';
 
 echo '<a class="surveyLink" href="/civics/">';
 	echo '<div class="pollInfoVotes">Votes<br />'.$this->surveys->civics->votes.'</div>';
-	echo 'Civics';
+	echo 'Vote on Civics';
 echo '</a>';
 
 echo '</div></div>';
@@ -22,12 +22,12 @@ echo '<div class="recentOrPopularBar">Round Two: 9/27 - 10/10</div>';
 
 echo '<a class="surveyLink" href="/spending/">';
 	echo '<div class="pollInfoVotes">Votes<br />'.$this->surveys->spending->votes.'</div>';
-	echo 'Spending';
+	echo 'Vote on Spending';
 echo '</a>';
 
 echo '<a class="surveyLink" href="/liveaction/">';
 	echo '<div class="pollInfoVotes">Votes<br />'.$this->surveys->liveaction->votes.'</div>';
-	echo 'Live Action';
+	echo 'Vote on Live Action';
 echo '</a>';
 
 echo '</div></div>';
