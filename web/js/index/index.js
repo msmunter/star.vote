d0575c77486a08c2594dc9017c00c3057c4a76f0
Steen Munter <msmunter@gmail.com>
2018-09-24 02:44:30 +0000

    One/two column auto switcher back in action.

--- web/js/index/index.js
+++ web/js/index/index.js
@@ -1,4 +1,18 @@
 /* Remember: poll.js gets included with the index */
 $(document).ready(function() {
+	adjustColumns();
+	$(window).resize(function() {
+		adjustColumns();
+	});
 	$('#pollQuestion').focus();
 });
+
+function adjustColumns() {
+	if ($(window).width() > 800) {
+		$('.columnOne').addClass('ui-block-a');
+		$('.columnTwo').addClass('ui-block-b');
+	} else {
+		$('.columnOne').removeClass('ui-block-a');
+		$('.columnTwo').removeClass('ui-block-b');
+	}
+}
