4f30fbe415fc8d683089aab4e00a89de0fe22558
Steen Munter <msmunter@gmail.com>
2020-04-19 19:06:00 +0000

    Basic API working.

--- controller/api.controller.php
+++ controller/api.controller.php
@@ -13,11 +13,23 @@ class ApiController extends Controller
 		$this->model->addEmail();
 	}
 
-	public function getnextemail()
+	public function v1()
 	{
 		$this->ajax = 1;
 		$this->doHeader = 0;
 		$this->doFooter = 0;
+		$this->apiVersion = 'v1';
+		if (strtolower($this->URLdata == 'getnextemail')) {
+			$this->getNextEmail('v1');
+		} else {
+			$returnArray['apiVersion'] = $this->apiVersion;
+			print json_encode($returnArray);
+		}
+	}
+
+	private function getNextEmail()
+	{
+		$return['apiVersion'] = $this->apiVersion;
 		$nextEmail = $this->model->getNextEmail();
 		if ($nextEmail) {
 			$return['requestId'] = $nextEmail['msgID'];
