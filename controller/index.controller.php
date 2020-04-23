b95deec976bb4a3e9bd6ce6538dbedc14b5628c1
Steen Munter <msmunter@gmail.com>
2020-04-23 01:19:15 +0000

    Added link on index for credentialing volunteers (once logged in).

--- controller/index.controller.php
+++ controller/index.controller.php
@@ -1,22 +1,15 @@
 <?php
 class IndexController extends Controller
 {
-	public $pollSet;
+	public $survey;
+	public $userCanValidate;
 	
 	public function index()
 	{
-		$oPoll = new PollController();
-		$mPoll = new PollModel;
-		$this->mostRecentPolls = $mPoll->getMostRecentPolls(0, 10);
-		$oPoll->processPollSet($this->mostRecentPolls);
-		$this->mostPopularPolls = $mPoll->getMostPopularPolls(0, 10);
-		$oPoll->processPollSet($this->mostPopularPolls);
-		unset($oPoll, $mPoll);
-		$oSurvey = new SurveyController();
 		$mSurvey = new SurveyModel();
-		$this->mostPopularSurveys = $mSurvey->getMostPopularSurveys(0, 10);
-		$this->mostRecentSurveys = $mSurvey->getMostRecentSurveys(0, 10);
-		unset($oSurvey, $mSurvey);
+		$this->survey = $mSurvey->getSurveyByCustomSlug('2020primary');
+		$this->userCanValidate = $mSurvey->userCanValidate($this->user->userID, $this->survey->surveyID);
+		unset($mSurvey);
 	}
 }
 ?>
--- view/index/index.view.php
+++ view/index/index.view.php
@@ -1,20 +1,11 @@
-<!-- <div id="indexHeaderContainer">
-	<div id="indexHeader">
-		<a href="https://www.starvoting.us">
-			<div id="indexHeaderText">
-				<div>Simple, fair voting.</div><div>Learn more:</div>
-			</div>
-			<div id="indexHeaderImageContainer">
-				<img id="starvotingLogo" src="web/images/starvoting_us_logo.png" />
-			</div>
-		</a>
-	</div>
-</div> -->
 <div class="bigContainer">
 	<div class="bigContainerTitle">IPO 2020 Primary</div>
 	<div id="indexCreatePollContainer">
-		<?php //include('view/poll/pollinputform.view.php'); ?>
 		<!-- <a class="ui-btn ui-btn-inline ui-mini ui-corner-all" href="/ipo2020/">Vote!</a> -->
-		Please visit <a href="https://register.ipo.vote/">register.ipo.vote</a> to acquire a ballot.
+		<?php if ($this->user->userID == 1 || $this->userCanValidate) { ?>
+			Hello. Perhaps you'd like to <a href="/survey/votervalidation/<?php echo $this->survey->surveyID; ?>/">validate some voters</a>?
+		<?php } else { ?>
+			Please visit <a href="https://register.ipo.vote/">register.ipo.vote</a> to acquire a ballot.
+		<?php } ?>
 	</div>
 </div>
