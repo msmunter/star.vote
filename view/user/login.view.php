331ef3ab44d5f1eac6520ce381cf02607b80862e
Steen Munter <msmunter@gmail.com>
2020-04-27 23:00:49 +0000

    Added precautionary language to login screen.

--- view/user/login.view.php
+++ view/user/login.view.php
@@ -4,8 +4,8 @@
 	<div class="bigContainerTitle">Login</div>
 	<div class="bigContainerInner">
 		<div class="ui-field-contain">
-			<label for="email">Email:</label>
-			<input type="text" name="email" id="email" placeholder="user@yourmail.domain" />
+			<label for="email">User:</label>
+			<input type="text" name="email" id="email" />
 		</div>
 		
 		<div class="ui-field-contain">
@@ -16,15 +16,23 @@
 		<div class="ui-field-contain">
 			<label for="authLength">Persistent Login:</label>
 			<select name="authLength" id="authLength" data-inline="true">
-				<option value="0">Session Only</option>
-				<option selected value="1">One Day</option>
-				<option value="7">One Week</option>
+				<option selected value="0">Session Only</option>
+				<option value="1">One Day</option>
+				<!-- <option value="7">One Week</option>
 				<option value="30">One Month</option>
-				<option value="365">One Year</option>
+				<option value="365">One Year</option> -->
 			</select>
 		</div>
+		<div>
+			By clicking "Accept & Login" below you agree to the following terms and conditions:
+			<ul>
+				<li>I understand and agree that all data and personal information provided or displayed to me while logged in to the STAR Elections Credentialing Committee application is private and personal data which is not to be shared, saved, displayed, sold, or used for any other purposes other than the credentialing of voters and the verifying of ballots cast in the Independent Party of Oregon primary and presidential preference poll.</li>
+				<li>I understand and agree that all data and personal information provided or displayed to me while verifying voters using STAR Elections voter data in the Oregon Secretary of State’s “My Vote” application, is private and personal data which is not to be shared, saved, displayed, sold, or used for any other purposes other than the credentialing of voters and the verifying of ballots cast in the Independent Party of Oregon primary and presidential preference poll.</li>
+			</ul>
+			STAR Elections takes the privacy of voter data very seriously. Thank you for your cooperation and thank you for your help.
+		</div>
 		<div class="center">
-			<button id="loginButton" data-inline="true" onclick="login()">Login</button>
+			<button id="loginButton" data-inline="true" onclick="login()">Accept & Login</button>
 		</div>
 	</div>
 </div>
