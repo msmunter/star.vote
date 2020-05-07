f5d962b2cb1ec12869b866b68b6ce205c8cf01ee
Steen Munter <msmunter@gmail.com>
2020-05-07 07:54:12 +0000

    Fixed image links for L2 validation.

--- view/survey/votervalfinal.view.php
+++ view/survey/votervalfinal.view.php
@@ -42,8 +42,8 @@
 							<tr><td></td><td id="validationComparisonTableCSZ"></td></tr>
 						</table>
 					</td><td id="validationComparisonImageCell">
-						<a id="validationImgHref1" href="/web/images/img_placeholder.svg" target="_blank"><img id="validationImg1" src="https://cdn.filestackcontent.com/<?php echo $this->voterIdent->cdnHandle1; ?>" /></a>
-						<a id="validationImgHref2" href="/web/images/img_placeholder.svg" target="_blank"><img id="validationImg2" src="https://cdn.filestackcontent.com/<?php echo $this->voterIdent->cdnHandle2; ?>" /></a>
+						<a id="validationImgHref1" href="https://cdn.filestackcontent.com/<?php echo $this->voterIdent->cdnHandle1; ?>" target="_blank"><img id="validationImg1" src="https://cdn.filestackcontent.com/<?php echo $this->voterIdent->cdnHandle1; ?>" alt="Validation Image 1" /></a>
+						<a id="validationImgHref2" href="https://cdn.filestackcontent.com/<?php echo $this->voterIdent->cdnHandle1; ?>" target="_blank"><img id="validationImg2" src="https://cdn.filestackcontent.com/<?php echo $this->voterIdent->cdnHandle2; ?>" alt="Validation Image 2" /></a>
 					</td></tr>
 				</table>
 				<?php if (in_array($this->voterIdent->verificationState, ['voted', 'rejectedOnce', 'acceptedOnce'])) { ?>
